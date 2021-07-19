bc3bbcbc564858f24f4f89e15700c394410b10f6
Edd Sansome <edward.sansome@smartpension.co.uk>
2021-07-19 17:12:57 +0100

    add params filtering

--- spec/services/get_movie_spec.rb
+++ spec/services/get_movie_spec.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
+Dotenv.load
 
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/vcr_cassettes'
   config.configure_rspec_metadata!
   config.hook_into :webmock
+  config.filter_sensitive_data('<🖕💩🖕>') { ENV['OMDB_API_KEY'] }
 end
 
 RSpec.describe GetMovie::ByTitle, :vcr do
   describe '#call' do
-    let(:movie) { subject.call('titanic') }
+    let(:movie) { GetMovie::ByTitle.call('titanic') }
 
     it 'should return a movie params' do
       expect(movie).to be_kind_of(Hash)
