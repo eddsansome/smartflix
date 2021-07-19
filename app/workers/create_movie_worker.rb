46f6fdfe6e478ae964a8b9433745aa42f30e32e1
Edd Sansome <edward.sansome@smartpension.co.uk>
2021-07-19 17:01:00 +0100

    refactored

--- app/workers/create_movie_worker.rb
+++ app/workers/create_movie_worker.rb
@@ -1,43 +1,13 @@
-require_relative "../../services/get_movie"
-
 class CreateMovieWorker
   include Sidekiq::Worker
   sidekiq_options retry: false, queue: 'movies'
 
   def perform(title)
-  sleep 5
 
-  movie_getter = Services::GetMovie.new
+    sleep 1
 
-  movie = movie_getter.by_title(title)
+    movie_params = GetMovie::ByTitle.call(title)
 
-  Movie.create({
-    title: movie.title,
-    year: movie.year,
-    rated: movie.rated,
-    released: movie.released,
-    runtime: movie.runtime,
-    genre: movie.genre,
-    director: movie.director,
-    writer: movie.writer,
-    # update this when we have actor model, here we convert to 
-    # a string, but we cast to array in the parser :smart_guy
-    actors: movie.actors.join,
-    plot: movie.plot,
-    language: movie.language,
-    country: movie.country,
-    awards: movie.awards,
-    poster: movie.poster,
-    metascore: movie.metascore,
-    imdbrating: movie.imdbrating,
-    imdbvotes: movie.imdbvotes,
-    imdbid: movie.imdbid,
-    # name clash - rename!
-    # type: movie.type,
-    dvd: movie.dvd,
-    boxoffice: movie.boxoffice,
-    production: movie.production,
-    website: movie.website
-  })
+    Movie.create!(movie_params)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
 require 'faker'
 
-# could change to csv / import
-
 100.times do
   CreateMovieWorker.perform_async(Faker::Movie.title)
 end
